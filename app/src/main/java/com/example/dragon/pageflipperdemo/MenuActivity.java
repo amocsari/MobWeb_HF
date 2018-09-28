8b3f90d18aeb2c6212cca0e92d86b7457110efe9
Mocsi <amocsari@gmail.com>
2018-09-28 22:43:21 +0200

    menu in

---
+++ app/src/main/java/com/example/dragon/pageflipperdemo/MenuActivity.java
@@ -0,0 +1,39 @@
+package com.example.dragon.pageflipperdemo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MenuActivity extends Activity {
+
+    Button bPageFlipDemo;
+    Button bPdfOpenDemo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        bPageFlipDemo = findViewById(R.id.bPageFlipDemo);
+        bPdfOpenDemo = findViewById(R.id.bPdfOpenDemo);
+
+        bPageFlipDemo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        bPdfOpenDemo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), PdfActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
